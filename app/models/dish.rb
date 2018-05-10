6b1b73f8342df2cdd0ed3e7b20dedcb764ab8028
Marcio Santos <mvs_designer@hotmail.com>
2018-05-10 18:59:10 -0300

    configurando models

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,6 @@
 class Dish < ApplicationRecord
+  validates_presence_of :name, :timePreparation, :price
+  belongs_to :restaurant
+  belongs_to :category
+  has_and_belongs_to_many :ingredients
 end
